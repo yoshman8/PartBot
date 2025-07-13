b15c1f56ce6de718edd64175c55ac1a379ef6bf4
Parth Mane <parth.mane@sprinklr.com>
2025-07-13 23:48:01 +0530

    web: Support static!

--- src/web/loaders/static.ts
+++ src/web/loaders/static.ts
@@ -1,8 +1,9 @@
-import { fsPath } from '@/utils/fsPath';
+import express, { type Application } from 'express';
 
-import type { Application } from 'express';
+import { fsPath } from '@/utils/fsPath';
 
 export default async function init(app: Application): Promise<void> {
 	app.get('/styles.css', (req, res) => res.sendFile(fsPath('web', 'react', 'compiled', 'styles.css')));
 	app.get('/favicon.ico', (req, res) => res.sendFile(fsPath('web', 'assets', 'favicon.ico')));
+	app.use('/static', express.static(fsPath('web', 'static')));
 }
