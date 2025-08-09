68db702b13da60eab6e67e669faad577c021c5ef
PartMan <parth.mane@sprinklr.com>
2025-08-10 02:37:14 +0530

    web: Purge CDN/NGINX configs

--- scripts/build_nginx_confs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-for file in src/web/configs/*; do
-  sed "s#{{PWD}}#$PWD#g;s#{{PORT}}#${PORT:-8800}#g" "$file" > "${file//.base/}"
-done
--- src/globals/patches.ts
+++ src/globals/patches.ts
@@ -8,8 +8,6 @@ const baseConsoleError = console.error.bind(console);
 const keyWarningPattern = 'Warning: Each child in a list should have a unique "key" prop.';
 const valueFieldPattern = 'Warning: You provided a `value` prop to a form field without an `onChange` handler.';
 
-// TODO: Patch out 'checked' warnings
-
 console.error = function (...args) {
 	if (typeof args[0] === 'string' && (args[0].startsWith(keyWarningPattern) || args[0].startsWith(valueFieldPattern))) {
 		return;
