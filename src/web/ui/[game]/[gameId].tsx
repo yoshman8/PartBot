fd2774c0eecd04bf2ba7b3c3b76fd939b9e1c231
PartMan <parth.mane@sprinklr.com>
2025-08-12 01:22:41 +0530

    web: Fallthrough for Othello

--- src/web/ui/[game]/[gameId].tsx
+++ src/web/ui/[game]/[gameId].tsx
@@ -1,9 +1,10 @@
 import { Games } from '@/ps/games';
+import { GamesList } from '@/ps/games/types';
 
 import type { UIRouteHandler } from '@/types/web';
 
 export const handler: UIRouteHandler = (req, res, next) => {
 	const { game: gameType } = req.params as { game: string; gameId: string };
-	if (!Object.keys(Games).includes(gameType)) return next();
+	if (!Object.keys(Games).includes(gameType) || ([GamesList.Othello] as string[]).includes(gameType)) return next();
 	res.getBundle(gameType, `${gameType.charAt(0).toUpperCase() + gameType.substring(1)} Replay`);
 };
