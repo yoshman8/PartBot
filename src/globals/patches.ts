8b88e2aec856b2481b315169661f645948bc615e
PartMan <parth.mane@sprinklr.com>
2025-08-14 11:33:46 +0530

    chore: Patch 'checked' React warnings

--- src/globals/patches.ts
+++ src/globals/patches.ts
@@ -5,11 +5,16 @@
  * @see https://github.com/facebook/react/issues/12567
  */
 const baseConsoleError = console.error.bind(console);
+
 const keyWarningPattern = 'Warning: Each child in a list should have a unique "key" prop.';
 const valueFieldPattern = 'Warning: You provided a `value` prop to a form field without an `onChange` handler.';
+const checkedFieldPattern = 'Warning: You provided a `value` prop to a form field without an `onChange` handler.';
 
 console.error = function (...args) {
-	if (typeof args[0] === 'string' && (args[0].startsWith(keyWarningPattern) || args[0].startsWith(valueFieldPattern))) {
+	if (
+		typeof args[0] === 'string' &&
+		[keyWarningPattern, valueFieldPattern, checkedFieldPattern].some(pattern => args[0].startsWith(pattern))
+	) {
 		return;
 	}
 	return baseConsoleError(...args);
