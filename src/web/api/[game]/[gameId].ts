c6895e9db88c94bd8a955067fba55516a115f300
PartMan <parth.mane@sprinklr.com>
2025-08-16 17:21:06 +0530

    web: Filter /api/[gameType]/[gameId] to only fire for gameType in GamesList

--- src/web/api/[game]/[gameId].ts
+++ src/web/api/[game]/[gameId].ts
@@ -1,12 +1,14 @@
 import { getGameById } from '@/database/games';
 import { IS_ENABLED } from '@/enabled';
+import { Games } from '@/ps/games';
 import { WebError } from '@/utils/webError';
 
 import type { RequestHandler } from 'express';
 
-export const handler: RequestHandler = async (req, res) => {
+export const handler: RequestHandler = async (req, res, next) => {
 	if (!IS_ENABLED.DB) throw new Error('Database is disabled.');
 	const { game: gameType, gameId } = req.params as { game: string; gameId: string };
+	if (!Object.keys(Games).includes(gameType)) return next();
 	try {
 		const game = await getGameById(gameType, gameId);
 		const serializable = game!.toJSON();
