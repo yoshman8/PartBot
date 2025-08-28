32d5f69fb264fe8887cbe039d3bca124f123600c
PartMan <parth.mane@sprinklr.com>
2025-08-28 11:48:35 +0530

    chore: Ping on Scavscord for Hunt Swap

--- src/ps/handlers/cron/ugo.ts
+++ src/ps/handlers/cron/ugo.ts
@@ -1,7 +1,10 @@
 import { resetUGOPlayed } from '@/cache/ugo';
+import Discord from '@/discord';
 import { TimeZone } from '@/ps/handlers/cron/constants';
+import getSecretFunction from '@/secrets/functions';
 
 import type { PSCronJobManager } from '@/ps/handlers/cron/index';
+import type { Client as DiscordClient } from 'discord.js';
 import type { Client } from 'ps-client';
 
 // UGO-CODE
@@ -10,5 +13,15 @@ export function register(this: Client, Jobs: PSCronJobManager): void {
 		resetUGOPlayed().then(backupUrl => {
 			this.getRoom('Board Games').send(`/modnote Reset player participation counts for the day! Backup: ${backupUrl}`);
 		});
+	});
+	// August 29, 22:00 GMT
+	Jobs.register('hunt-swap-ping', '0 22 29 08 *', TimeZone.GMT, () => {
+		getSecretFunction<(client: DiscordClient) => Promise<void>>('pingForHuntSwap', async () => {})(Discord)
+			.then(() => {
+				this.getRoom('Scavengers').send('/modnote Pinged for hunt swap. I think. Probably. I hope.');
+			})
+			.catch(() => {
+				this.getRoom('Scavengers').send('/modnote Unable to ping for hunt swap! Tag PartMan!');
+			});
 	});
 }
